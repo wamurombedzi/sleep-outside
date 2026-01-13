56b0ef60f4330c156aeb12a8c499337305513bf0
king6ava9e <acheampongkingsley2003@gmail.com>
2026-01-13 10:48:01 +0000

    worked on my product .js bug fix

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -7,6 +7,7 @@ export function qs(selector, parent = document) {
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
+
   return JSON.parse(localStorage.getItem(key));
 }
 // save data to local storage
