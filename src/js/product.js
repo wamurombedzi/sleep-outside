e038eeee6d9f5ca59ec9024a2222ea0b05c1dc98
Jerjes Mariluz <juniormariluz1@gmail.com>
2026-01-16 10:38:58 -0500

    feat: Updating product logic for week 1.

--- src/js/product.js
+++ src/js/product.js
@@ -8,7 +8,7 @@ function addProductToCart(product) {
   cartItems.push(product);
   setLocalStorage('so-cart', cartItems);
 }
-// add to cart button event handler
+// add to cart button event handler:
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
