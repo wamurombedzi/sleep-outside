95d9e3acdaf3ff89bf7d8da6ac07d7b579b45d60
Jerjes Mariluz <juniormariluz1@gmail.com>
2026-01-31 23:26:26 -0500

    feat: complete checkout process and form validation.

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,2 +1,16 @@
 import { loadHeaderFooter } from './utils.mjs';
+import CheckoutProcess from './CheckoutProcess.mjs';
+
 loadHeaderFooter();
+
+const myCheckout = new CheckoutProcess('so-cart', '#order-summary');
+myCheckout.init();
+
+document.querySelector('#zip').addEventListener('blur', () => {
+  myCheckout.calculateOrderTotal();
+});
+
+document.forms['checkout'].addEventListener('submit', (e) => {
+  e.preventDefault();
+  myCheckout.checkout();
+});
